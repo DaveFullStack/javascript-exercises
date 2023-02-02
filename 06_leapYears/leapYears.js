ec57d3ad49264d5384a1f064b185abeb589a2770
DaveFullStack <119569407+DaveFullStack@users.noreply.github.com>
2023-02-02 19:18:47 +0000

    completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+let a = year;
+
+
+if (a % 400 === 0) {
+    return true
+}
+else if (a % 100 === 0){
+    return false;
+}
+else if (a % 4 === 0) {
+    return true;
+}
+else {
+    return false;
+}
 
 };
 
