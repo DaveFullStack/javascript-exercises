85fca7e4dc3c31cb6f1551bc15da94ab09acb534
DaveFullStack <119569407+DaveFullStack@users.noreply.github.com>
2023-02-02 21:36:53 +0000

    completed add and subtract. finishing off for the night.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,12 +1,30 @@
-const add = function() {
+const add = function(...addNumbers) {
+  let add = addNumbers;
+  let result = 0;
+  for (i = 0; i < add.length; i++) {
+    result = result + add[i];
+  }
+  return result;	
+};
+
+const subtract = function(...subtractNumbers) {
+  let sub = subtractNumbers;
+  let result = sub[0];
+  for (i = 1; i < sub.length; i++) {
+    result = result - sub[i];
+  }
+  parseFloat(result);
+  return result;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
+const sum = function(...sumNumbers) {
+  let sum = sumNumbers;
+  let result = 0;
+  for (i = 0; i < sum.length; i++) {
+    result = result + sum[i];
+  }
+  return result;
 	
 };
 
