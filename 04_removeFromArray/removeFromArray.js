51f991ec77095456d5e4ac2a02b627be36b5b882
DaveFullStack <119569407+DaveFullStack@users.noreply.github.com>
2023-02-02 16:21:24 +0000

    not entirely sure how this one works. need to revisit arrays

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(mainArray, ...args) {
+    let a = mainArray;
+    let b = args;
+    let c = a.filter(d => !b.includes(d));
+    return c;
+    
 
+    //main array contains all values
+    //args contains all arguments stored in array
+    //need to say if any values in args is equal to values in main array
+    //it removes the value from the main array
 };
 
 // Do not edit below this line
