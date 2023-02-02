9d5dd9cb1b82ee7053b6010a111c0325a45b835e
DaveFullStack <119569407+DaveFullStack@users.noreply.github.com>
2023-02-02 15:14:46 +0000

    completed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(reverse) {
+    let charArray = Array.from(reverse)
+    let output = '';
+    for (i = charArray.length -1; i >= 0; i--) {
+        output += charArray[i];
+    }
+    return output;
 };
 
 // Do not edit below this line
