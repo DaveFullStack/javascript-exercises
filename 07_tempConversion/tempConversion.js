3375dd6e4b100580ed54ac72b1b04a3e471a97ff
DaveFullStack <119569407+DaveFullStack@users.noreply.github.com>
2023-02-02 20:47:46 +0000

    completed exercise 07. convertToCelcius using toFixed function and parseFloat to return as an int. using Math.round for convertToFarenheit funtion. takes farenheit variabl, multiplies by 10 the divides by 10 to return first decimal place.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,16 @@
 const convertToCelsius = function(fahrenheit) {
   let a = fahrenheit;
   let celcius = (a - 32) * (5 / 9);
-  return Math.round(celcius);
+  let result = celcius.toFixed(1);
+  result = parseFloat(result)
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let a = celcius;
+  let fahrenheit = a * (9/5) + 32;
+  let result = Math.round(fahrenheit *10) / 10;
+  return result;
 };
 
 // Do not edit below this line
