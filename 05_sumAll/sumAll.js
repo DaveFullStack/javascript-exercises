5e4b0176b7357e5e1056c0532d2274e5b570269a
DaveFullStack <119569407+DaveFullStack@users.noreply.github.com>
2023-02-02 17:40:58 +0000

    completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,36 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let a = numOne;
+    let b = numTwo;
+    let result = 0;
+    let error = 'ERROR'
 
-};
+    if (typeof a !== "number" || typeof b !== "number") {
+        return error
+    }
+
+    else {
+        if (a < b) {
+            while (a <= b) {
+                result += a;
+                a++;
+                }
+            }
+        else {
+            while (a >= b) {
+                result += a;
+                a--;
+            }
+        }
+
+    
+    if (result > 0) {
+        return result;
+        }
+    else if (result < 0) {
+        return error;
+        }   
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
