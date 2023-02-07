1b106015893c466c0283801ac8c37a837c13eeb5
DaveFullStack <119569407+DaveFullStack@users.noreply.github.com>
2023-02-07 15:25:46 +0000

    completed exercise 09. Need to look more into regular experssions and how they're used

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (palindrome) {
+    
+  var removeChar = palindrome.replace(/[^A-Z0-9]/ig, "").toLowerCase();
 
-};
+  
+  var checkPalindrome = removeChar.split('').reverse().join('');
+
+   if(removeChar === checkPalindrome){
+     return true;
+   }else{
+     return false;
+   }
+}
 
 // Do not edit below this line
 module.exports = palindromes;
