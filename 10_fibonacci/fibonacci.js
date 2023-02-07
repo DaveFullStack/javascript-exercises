3844dd725865cba16770b99d72190e36a161e2b4
DaveFullStack <119569407+DaveFullStack@users.noreply.github.com>
2023-02-07 15:40:55 +0000

    completed exercise 10.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(fibonacci) {
+
+    let a = fibonacci;
+    let numOne = 0, numTwo = 1, nextNum;
+    if (a < 0) {
+        return 'OOPS'
+    }
+    else if (a == 1) {
+        return 1;
+    }
+    for (i = 0; i < a -1; i++) {
+        nextNum = numOne + numTwo;
+        numOne = numTwo;
+        numTwo = nextNum;
+    }
+    return nextNum;
+
+
+
 
 };
 
